23b30be13b5605d22f6ac88c63332d8c9718fc8b
viktoriaas <spisakova@ics.muni.cz>
2024-12-19 12:19:10 +0100

    fix sh script

--- create_config_maps.sh
+++ create_config_maps.sh
@@ -3,7 +3,7 @@
 npm install
 npm run build
 
-NAMESPACE="jupyterhub-dev-ns"
+NAMESPACE="jupyterhub-ns"
 BASE_DIR="./dist"
 
 create_configmap() {
@@ -11,10 +11,15 @@ create_configmap() {
   local path=$2
 
   # applying the ConfigMaps directly to cluster
-  kubectl delete configmap $name --namespace $NAMESPACE --ignore-not-found
-  kubectl create configmap $name --from-file=$path --namespace $NAMESPACE
+
+  # don't delete configmaps from cluster automatically! if ns changes, it could lead to catastrophe
+  #  kubectl delete configmap $name --namespace $NAMESPACE --ignore-not-found
+  #  kubectl create configmap $name --from-file=$path --namespace $NAMESPACE
   # creating the ConfigMaps locally
-  kubectl create configmap $name --from-file=$path --dry-run=client -o yaml > $name.yaml
+  kubectl create configmap $name --from-file=$path --dry-run=client -o json > $name.json
+  kubectl patch --local -f $name.json --type=json -p='[{"op": "remove", "path": "/metadata/creationTimestamp"}]' -o yaml > $name.yaml
+  rm $name.json
+
 }
 
 create_configmap "static-files" "$BASE_DIR"
@@ -22,6 +27,4 @@ create_configmap "static-files-js" "$BASE_DIR/static/custom-js"
 create_configmap "static-files-css" "$BASE_DIR/static/custom-css"
 create_configmap "static-files-woff" "$BASE_DIR/static/woff"
 create_configmap "static-files-woff2" "$BASE_DIR/static/woff2"
-create_configmap "static-files-images" "$BASE_DIR/static"
-
 echo "All ConfigMaps processed successfully."
