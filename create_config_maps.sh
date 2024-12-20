887947392f1b2df100b69a362eed848a0b9d4f9d
viktoriaas <spisakova@ics.muni.cz>
2024-12-20 16:26:17 +0100

    params to .sh

--- create_config_maps.sh
+++ create_config_maps.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]; then
+  echo "Usage: $0 namespace_of_hub"
+  exit 1
+fi
+
+NAMESPACE=$1
+
 npm install
 npm run build
 
-NAMESPACE="jupyterhub-ns"
+#NAMESPACE="jupyterhub-dev-ns"
 BASE_DIR="./dist"
 
 create_configmap() {
   local name=$1
   local path=$2
 
-    # don't delete configmaps from cluster automatically! if ns changes, it could lead to catastrophe
-  #  kubectl delete configmap $name --namespace $NAMESPACE --ignore-not-found
-  #  kubectl create configmap $name --from-file=$path --namespace $NAMESPACE
-  # creating the ConfigMaps locally
-  kubectl create configmap $name --from-file=$path --dry-run=client -o json > $name.json
-  kubectl patch --local -f $name.json --type=json -p='[{"op": "remove", "path": "/metadata/creationTimestamp"}]' -o yaml > $name.yaml
-  rm $name.json
+  # don't delete configmaps from cluster automatically! if ns changes, it could lead to catastrophe
+  if [ "$NAMESPACE" = "jupyterhub-dev-ns" ]; then
+    kubectl delete configmap $name --namespace $NAMESPACE --ignore-not-found
+    kubectl create configmap $name --from-file=$path --namespace $NAMESPACE
+  else
+    # creating the ConfigMaps locally
+    kubectl create configmap $name --from-file=$path --dry-run=client -o json > $name.json
+    kubectl patch --local -f $name.json --type=json -p='[{"op": "remove", "path": "/metadata/creationTimestamp"}]' -o yaml > $name.yaml
+    rm $name.json
+  fi
 
 }
 
