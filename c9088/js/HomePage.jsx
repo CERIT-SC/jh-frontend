bee3d0d996dfa0e3cdf9a23908e5a2051cd30517
Adam Paulen <xpaulen@fi.muni.cz>
2025-01-23 16:37:39 +0100

    commented out dev feature

--- c9088/js/HomePage.jsx
+++ c9088/js/HomePage.jsx
@@ -8,35 +8,35 @@ import JupyterHubHeader from "../../src/components/FooterAndHeader/JupyterHubHea
 
 function HomePage() {
   // for testing with npm run dev please uncomment this block
-  const appConfig = {
-    spawners: {
-      "test": {
-        last_activity: "2024-11-24T15:48:29.604740Z",
-        url: "/user/test",
-        active: true,
-        ready: false,
-      },
-      "test1": {
-        last_activity: "2024-11-24T15:46:56.719146Z",
-        url: "/user/test1",
-        active: false,
-        ready: false,
-      },
-      ...Array.from({ length: 0 }, (_, i) => `spawner${i + 1}`).reduce((acc, spawner) => {
-        acc[spawner] = {
-          last_activity: new Date().toISOString(),
-          url: `/user/${spawner}`,
-          active: Math.random() < 0.5, // Randomly set active status
-          ready: Math.random() < 0.5, // Randomly set ready status
-        };
-        return acc;
-      }, {})
-    },
-    default_server_active: false,
-    url: "http://localhost",
-    userName: "dev",
-    xsrf: "sample-xsrf-token",
-  };
+  // const appConfig = {
+  //   spawners: {
+  //     "test": {
+  //       last_activity: "2024-11-24T15:48:29.604740Z",
+  //       url: "/user/test",
+  //       active: true,
+  //       ready: false,
+  //     },
+  //     "test1": {
+  //       last_activity: "2024-11-24T15:46:56.719146Z",
+  //       url: "/user/test1",
+  //       active: false,
+  //       ready: false,
+  //     },
+  //     ...Array.from({ length: 0 }, (_, i) => `spawner${i + 1}`).reduce((acc, spawner) => {
+  //       acc[spawner] = {
+  //         last_activity: new Date().toISOString(),
+  //         url: `/user/${spawner}`,
+  //         active: Math.random() < 0.5, // Randomly set active status
+  //         ready: Math.random() < 0.5, // Randomly set ready status
+  //       };
+  //       return acc;
+  //     }, {})
+  //   },
+  //   default_server_active: false,
+  //   url: "http://localhost",
+  //   userName: "dev",
+  //   xsrf: "sample-xsrf-token",
+  // };
 
   const [defaultServerActive, setDefaultServerActive] = useState(
     appConfig.default_server_active,
