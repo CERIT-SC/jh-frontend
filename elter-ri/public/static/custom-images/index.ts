9ba0e1276e8ebbec42a106931b90efeaa4e33589
xbencs <xbencs00@vutbr.cz>
2026-07-23 15:56:37 +0200

    chore: update bun.lock and add elter-ri public assets

---
+++ elter-ri/public/static/custom-images/index.ts
@@ -0,0 +1,3 @@
+export { default as elterLogo } from "./elter_logo.svg";
+export { default as elterLogoLeaf } from "./eLTER_logo_leaf.svg";
+export { default as elterLogoLeafAlt } from "./eLTER_logo_leaf-1.svg";
